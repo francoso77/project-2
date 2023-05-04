7a760a322e48b77f3d92facfe8aeda3ea41f9036
francoso77 <frank77invest@gmai.com>
2023-05-04 15:47:26 -0300

    hooks up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    //setCounter(counter + 1);
+    setCounter((c) => c + 1);
+  };
+
+  //componentDidUpdate --> executa toda vez que o component atualiza
+
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  //componentDidMount --> executa UMA vez qdo renderiza
+
+  useEffect(() => {
+    console.log('componentDidMount');
+    //o segundo paramentro uma array de DEPENDENCIAS
+    //para exucutar uma vez vai vazio
+  }, []);
+
+  useEffect(() => {
+    console.log('Unsando Array de dependencias');
+    //se usar uma variável tipo COUNTER usar a base no []
+  }, [counter == 10]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contador: {counter}</h1>
+      <button type="button" onClick={handleClick}>
+        +
+      </button>
     </div>
   );
 }
