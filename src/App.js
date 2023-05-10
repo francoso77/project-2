874c110ed01b6d545984528733bd1aceebb34db2
francoso77 <frank77invest@gmai.com>
2023-05-10 17:30:25 -0300

    useRef + focus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import P from 'prop-types';
 import './App.css';
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho, renderizou!');
   return (
     <div className="post" key={post.id}>
-      <h1>{post.title}</h1>
+      {/*preciso passar uma função anonima por causa do paramentro*/}
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,26 +21,35 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const inputRef = useRef(null);
 
   console.log('Componente Pai Renderizou');
 
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [value]);
 
   return (
     <div className="App">
       <p>
         <input
+          ref={inputRef}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -47,7 +59,7 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} />;
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
           })
         );
       }, [posts])}
