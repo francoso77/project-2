415aa8e014b284a5be06918e145812c509e8a18a
francoso77 <frank77invest@gmai.com>
2023-05-16 11:29:59 -0300

    mais de 1 context

--- src/tamplates/App/index.jsx
+++ src/tamplates/App/index.jsx
@@ -1,13 +1,16 @@
 import { Posts } from '../../components/Posts';
+import { CounterProvider } from '../../contexts/CounterProvider';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import './styles.css';
 
 export default function App() {
   return (
-    <PostsProvider>
-      <div>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
